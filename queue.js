b3d325462bb96abe5eb5647586bed18fcbf23919
Adam Bradbury <f.adambrad@gmail.com>
2017-12-15 10:54:48 -0700

    queue.js fully functioning

--- queue.js
+++ queue.js
@@ -25,5 +25,42 @@ class Queue {
             this.first = node;
         }
     }
-            
-}
+
+    dequeue() {
+        if(this.first === null) {
+            return 
+        }
+
+        let node = this.first
+        this.first = node.prev
+        if(node === this.last){
+            this.last = null
+        }
+        return node.data
+    }  
+    
+    display(){
+        let node = this.first
+        while(node !== null){
+            console.log(node.data)
+            node = node.prev
+        }
+    }
+ }
+
+
+ const q = new Queue()
+
+ q.enqueue('Quang')
+ q.enqueue('Adam')
+//  q.enqueue('Sara')
+//  q.enqueue('Batman')
+//  q.enqueue('Dennis')
+//  q.enqueue('Moon Man')
+//  q.enqueue('Ho')
+
+// console.log(q.dequeue(), 'dequeue')
+// console.log(q.dequeue(), 'dequeue')
+q.display();
+console.log(q)
+
