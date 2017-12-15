b5abf67988bdcd839e2e9b424cadfbcf4064651b
Adam Bradbury <f.adambrad@gmail.com>
2017-12-15 09:58:02 -0700

    finished stack class fns

--- stack.js
+++ stack.js
@@ -21,4 +21,41 @@ class Stack {
               this.top = node;  
              
     }
+
+    peek(){
+        if(this.top === null){
+            return null
+        }
+        return this.top.data
+    }
+
+    pop(){
+        const node = this.top
+        this.top = node.next
+        return node.data
+    }
+
+    display(){
+        let node = this.top
+        while(node !== null){
+        console.log(node.data, 'display')
+        node = node.next
+        }
+    }
 }
+
+const s = new Stack()
+
+s.push(0)
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(4)
+s.push(5)
+
+// console.log(s.peek())
+// console.log(s.pop())
+
+s.display()
+
+console.log(s)
